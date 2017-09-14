fbd3a0da621425cae389e7ed0743b3b4260fb39c
Bruno Guedes <brunoguedes@brunos-macbook-pro.home>
2017-09-14 17:10:33 +0200

    Forcing one Query Server usage

--- src/main/java/com/rest/couchbase/config/Database.java
+++ src/main/java/com/rest/couchbase/config/Database.java
@@ -3,6 +3,9 @@ package com.rest.couchbase.config;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.env.CouchbaseEnvironment;
+import com.couchbase.client.java.env.DefaultCouchbaseEnvironment;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,25 +13,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Database {
 
-    @Value("${storage.host}")
-    private String host;
+	@Value("${storage.host}")
+	private String host;
 
-    @Value("${storage.bucket}")
-    private String bucket;
+	@Value("${storage.bucket}")
+	private String bucket;
 
-    @Value("${storage.username}")
-    private String username;
+	@Value("${storage.username}")
+	private String username;
 
-    @Value("${storage.password}")
-    private String password;
+	@Value("${storage.password}")
+	private String password;
 
-    public @Bean Cluster couchbaseCluster() {
-        CouchbaseCluster cluster = CouchbaseCluster.create(host);
-        //cluster.authenticate(username, password);
-        return cluster;
-    }
+	public @Bean Cluster couchbaseCluster() {
 
-    public @Bean Bucket loginBucket() {
-        return couchbaseCluster().openBucket(bucket);
-    }
+		CouchbaseEnvironment env = DefaultCouchbaseEnvironment.builder().queryEndpoints(1).build();
+
+		CouchbaseCluster cluster = CouchbaseCluster.create(env, host);
+		// cluster.authenticate(username, password);
+		return cluster;
+	}
+
+	public @Bean Bucket loginBucket() {
+		return couchbaseCluster().openBucket(bucket);
+	}
 }
